b1cda2a74d0c0e6fb2139307e02a00918e7aa509
Manny <vmlujanjr@outlook.com>
2022-03-28 23:19:59 -0500

    implemented a function to retrieve user repo data from github

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,30 @@
-var getUserRepos = function () {
-    console.log("function was called!");
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+    // format the github api url
+    const apiUrl = ("https://api.github.com/users/" + user + "/repos");
+
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+// example
+// var getUserRepos = function (user) {
+//     console.log("function was called!");
+
+//     fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+//         console.log("got it", response)
+//     });
+
+//     fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+//         response.json().then(function(data) {
+//             console.log(data);
+//         });
+//     });
+
+//     console.log("outside");
+// };
+
+getUserRepos("vmlujanjr");
